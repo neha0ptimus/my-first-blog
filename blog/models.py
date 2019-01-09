818e26f8268011f99e83ffb58f4302d804ad9c1a
Neha Bora <nehabora@0ptiadmins-MacBook-Pro-3.local>
2019-01-09 14:04:03 -0500

    Modified templates to display posts from database

--- blog/models.py
+++ blog/models.py
@@ -15,10 +15,11 @@ class Post(models.Model):
 
 
     def publish(self):
-        self.published_date = timezone.now
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
         return self.title
+
 
     
